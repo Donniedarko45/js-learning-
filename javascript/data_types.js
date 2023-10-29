a0e18651855464af1444c210f6c58e42dd0f8623
Kartikey Pandey <134577536+Donniedarko45@users.noreply.github.com>
2023-10-29 19:50:39 +0000

    data_types

---
+++ javascript/data_types.js
@@ -0,0 +1,26 @@
+"use strict";  //iska matlab treat all js code as newer version
+
+//alert(3+3) we are using nodejs not browser
+
+console.log(3+9)
+console.log("kartikey")
+//for documentation use mdn(unofficial) or tc39.
+
+let name = "kartikey" //string data type "" issi me likha jata h
+let age = 18  //number data type
+let isheloggedin = false //boolean data type
+
+// number range=> 2 to the power 53 for greater number use bigint data type
+// boolean => true/false
+// null => standalone value
+//  undefined => value assign nai hui h
+// symbol react ka use me unique
+
+// object
+let num=169
+
+console.log(typeof "hitesh")
+console.log(typeof num);
+console.log(typeof null); //object h isska type
+console.log(typeof undefined); // undefined
+
