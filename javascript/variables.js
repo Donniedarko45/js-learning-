99f61e74d49a223935cf7f2f90eebe746b7b06f5
Kartikey Pandey <134577536+Donniedarko45@users.noreply.github.com>
2023-10-29 19:51:17 +0000

    variables

---
+++ javascript/variables.js
@@ -0,0 +1,21 @@
+const account_id = 144553 // for declaration of constant we use const keyword
+
+// var keyword ka use nai krna h agr hum var keyword ka use kiye aur ekk code me koi naya line baad me lagaya same name se var use karke toh sabme wai latest value change ho jati thi isliye hum  let naam ka keyword use karenge.
+
+let account_email = "kartikey224252@gmail.com" //use let instead of var
+var account_password = "me"
+account_city =  "dehradun" //bina kissi keyword ka use kiye bhi use kar skte h par ye tareeka acha nahi hai
+let account_state
+/*
+prefer not to use var because of issue var because of issue in block scope and functional scope
+
+*/
+
+// account_id = 2 not alloewed
+
+account_email = "hihiii@gmail.com"
+var account_password = "me34"
+account_city =  "basti"
+
+console.log(account_id);
+console.table([account_email,account_password,account_city,account_state]);   //account_state me kuch nai likha h isliye undefined dikha rha h.
